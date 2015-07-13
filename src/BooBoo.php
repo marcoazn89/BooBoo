19df17695eb671e02b73d700ee866594d2344c16
Marco Chang <imnotmarco@live.com>
2015-07-13 17:50:59 -0400

    Removed shouldLog for non booboo exceptions

--- src/BooBoo.php
+++ src/BooBoo.php
@@ -149,9 +149,7 @@ class BooBoo extends \Exception {
 	 */
 	final public static function exceptionHandler($exception) {
 		if(get_class($exception) !== __CLASS__) {
-			if(self::$booboo->shouldLog()) {
-				self::$booboo->getLogger()->log(get_class($exception).": {$exception->getMessage()} in {$exception->getFile()} at line {$exception->getLine()}. Stack trace: {$exception->getTraceAsString()}");
-			}
+			self::$booboo->getLogger()->log(get_class($exception).": {$exception->getMessage()} in {$exception->getFile()} at line {$exception->getLine()}. Stack trace: {$exception->getTraceAsString()}");
 
 			$format = ContentType::getInstance()->getString();
 
